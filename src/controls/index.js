cfe5d70fa4fd23c74cfaebfec6e5895cb7f3a82c
manobala-selvaraj-crayonte <56626600+manobala-selvaraj-crayonte@users.noreply.github.com>
2019-10-22 12:34:05 +0530

    Update index.js

--- src/controls/index.js
+++ src/controls/index.js
@@ -1,4 +1,5 @@
 export { default as ValueEditor } from './ValueEditor';
 export { default as ValueSelector } from './ValueSelector';
+export { default as ValueInput } from './ValueInput';
 export { default as ActionElement } from './ActionElement';
 export { default as NotToggle } from './NotToggle';
